29ac0e50c66502a241e5d912ed2fb3b2f7a2d25a
Nicolas <nicolas.brazzolotto@edu.devinci.fr>
2020-09-24 16:07:31 +0200

    ADD Factories and Seeds

---
+++ database/factories/ProductFactory.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Product;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class ProductFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Product::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+
+        $category = DB::table('categories')->select('id')->inRandomOrder()->limit(1)->value('id');
+
+        return [
+
+            "name" => $this->faker->name,
+            "image" => $this->faker->imageUrl(640, 480),
+            "price" => rand(0, 999),
+            "description" => $this->faker->realText(150),
+            "stock" => rand(0, 999),
+            "category_id" => $category,
+
+        ];
+    }
+}
---
+++ database/seeders/CategoriesTableSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Eloquent\Model;
+
+class CategoriesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Model::unguard();
+
+        Schema::disableForeignKeyConstraints();
+        DB::table('categories')->truncate();
+
+        Category::factory()->times(10)->create();
+
+        Schema::enableForeignKeyConstraints();
+        Model::reguard();
+    }
+}
