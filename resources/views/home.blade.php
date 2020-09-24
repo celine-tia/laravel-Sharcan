df8527071be316f4d9a762e47fb912ac12c5ba32
Nicolas <nicolas.brazzolotto@edu.devinci.fr>
2020-09-24 14:35:34 +0200

    ADD Bootstrap Vue AND ADD Product/Category Model

---
+++ resources/views/home.blade.php
@@ -0,0 +1,15 @@
+@extends('layout')
+
+
+
+@section('main')
+
+
+
+    <h1>Hello, world!</h1>
+
+
+
+@endsection
+
+
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,17 @@
+<!doctype html>
+<html lang="en">
+<head>
+    @include('layout.partials.head')
+</head>
+<body>
+
+    @include('layout.partials.header')
+
+
+    @yield('main')
+
+
+    @include('layout.partials.footer')
+
+</body>
+</html>
